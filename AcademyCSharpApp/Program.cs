91e8d41dbe0ae8369dced4d91c5eca6c0e4aa0cd
melekkarakoc77-droid <melekkarakoc77@gmail.com>
2026-07-14 13:29:30 +0300

    fease implement static e-commerce shipping check with if-else

--- AcademyCSharpApp/Program.cs
+++ AcademyCSharpApp/Program.cs
@@ -1,24 +1,31 @@
-﻿//PROJECT: E-COMMERCE CHECKOUT CALCULATOR
-string productName="Wriless Headphones";
-double productPrice=79.99;
-int quantity=2;
-double discountAmount=15.00;
-double taxRate=0.18;
-int friendsSharingBill=2;
-double subtotal=quantity * productPrice;
-double discountedAmount= subtotal - discountAmount;
-double taxAmount= discountedAmount * taxRate;
-double finalTotal =discountedAmount+taxAmount;
-double costPerPerson= finalTotal / friendsSharingBill;
-Console.WriteLine("=== RECEIPT ===");
-Console.WriteLine("Product: " + productName);
-Console.WriteLine("Price: $" + productPrice);
-Console.WriteLine("Quantity: " + quantity);
-Console.WriteLine("-------------------------");
-Console.WriteLine("Subtotal: $" + subtotal);
-Console.WriteLine("Discount Applied: -$" + discountAmount);
-Console.WriteLine("Tax (18%): $" + taxAmount);
-Console.WriteLine("-------------------------");
-Console.WriteLine("GRAND TOTAL: $" + finalTotal);
-Console.WriteLine("Each person pays (" + friendsSharingBill + " people): $" + costPerPerson);
-Console.WriteLine("===============");
+﻿// PROJECT: SMART SHIPPING & DISCOUNT SYSTEM 
+using System.Diagnostics.CodeAnalysis;
+
+double cartTotal=120.00;
+double finalTotal=cartTotal;
+double shippingCost=0.00;
+Console.WriteLine("original cart total: ${cartTotal}");
+Console.WriteLine("---------------------------------");
+if (cartTotal>=100.00)
+{
+    Console.WriteLine("condition met:cart is $100 or more");
+    Console.WriteLine("result:you got %10 dicount and free shipping");
+    double discount=cartTotal * 0.10;
+    finalTotal=cartTotal - discount;
+}
+else if(cartTotal>=50.00)
+{
+    Console.WriteLine("condition met:cart s between $50 and $100");
+    Console.WriteLine("result:you got free shipping");
+}
+else
+{
+    Console.WriteLine("condition met:cart is under $50");
+    Console.WriteLine("result:shipping cost is $9.99");
+
+    shippingCost=9.99;
+    finalTotal=cartTotal+shippingCost;
+}
+Console.WriteLine("--------------------------------");
+Console.WriteLine($"Shipping Cost: ${shippingCost}");
+Console.WriteLine($"YOUR FINAL TOTAL IS: ${finalTotal}");
