23cd047079ad2e35d7fa32f7a48c991284682743
melekkarakoc77-droid <melekkarakoc77@gmail.com>
2026-07-14 22:18:10 +0300

    feat:design first Product class and instantiate objects in C#

--- AcademyCSharpApp/Program.cs
+++ AcademyCSharpApp/Program.cs
@@ -1,27 +1,18 @@
-﻿// PROJECT: SHIFTING TO METHODS
-double shoesPrice = 120.00;
-double discountRate = 0.15;
-double discountAmount = CalculateDiscount(shoesPrice, discountRate);
-double discountedPrice = shoesPrice - discountAmount;
-double taxAmount = CalculateTax(discountedPrice);
-double finalTotal = discountedPrice + taxAmount;
-PrintReceipt(shoesPrice, discountAmount, taxAmount, finalTotal);
-static double CalculateDiscount(double price,double rate)
+﻿// PROJECT: MY FIRST OOP CLASS & OBJECT
+Product product1 = new Product();
+product1.id = 1;
+product1.name="wireless mouse";
+product1.price=29.99;
+Product product2 = new Product();
+product2.id=2;
+product2.name="mechanical keyboard";
+product2.price=89.99;
+Console.WriteLine("=== PRODUCT LIST FROM OOP ===");
+Console.WriteLine($"product1: {product1.name} costs {product1.price}");
+Console.WriteLine($"product2 : {product2.name} costs {product2.price}");
+class Product
 {
-  return price * rate;
-}
-static double CalculateTax(double price)
-{
-    double taxRate = 0.18;
-    return price * taxRate;
-}
-static void PrintReceipt(double original,double discount,double tax,double total)
-{
-    Console.WriteLine("=== RECEIPT WITH METHODS ===");
-    Console.WriteLine($"Original Price: ${original}");
-    Console.WriteLine($"Discount Amount: -${discount}");
-    Console.WriteLine($"Tax (18%): ${tax}");
-    Console.WriteLine("----------------------------");
-    Console.WriteLine($"GRAND TOTAL: ${total}");
-    Console.WriteLine("============================");
+    public int id;
+    public string name="";
+    public double price;
 }
