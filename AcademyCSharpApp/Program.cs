ab14c88b031ea516a0f850abca61fb499bc32f02
melekkarakoc77-droid <melekkarakoc77@gmail.com>
2026-07-14 12:07:34 +0300

    feat:declare and print basic variables

---
+++ AcademyCSharpApp/Program.cs
@@ -0,0 +1,8 @@
+﻿int studentAge=22;
+double studentGpa=3.06;
+string studentName="Melek Nur";
+bool isStudentActive=true;
+Console.WriteLine("student name:"+ studentName);
+Console.WriteLine("age:"+studentAge);
+Console.WriteLine("GPA:"+studentGpa);
+Console.WriteLine("is active :" + isStudentActive);
